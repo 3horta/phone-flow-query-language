5042bfaf222252ba9d508aa7f9f6ee49e155bcf9
hbm99 <ernestoblancomalmierca@gmail.com>
2022-12-03 00:30:33 -0500

    Add set operations, filter method

--- api/pfql_api.py
+++ api/pfql_api.py
@@ -1,6 +1,8 @@
-from typing import List
+from typing import List, Tuple
 import pandas as pd
 import datetime as dt
+
+#region classes
 
 class Time:
     def __init__(self, beginning_time : dt.datetime, ending_time : dt.datetime = None) -> None:
@@ -21,12 +23,54 @@ class Time:
             interval = pd.Interval(pd.Timestamp(beginning_time), pd.Timestamp(ending_time), closed = 'left')
         return interval
 
-def __towers_location_dataframes():
+#endregion
+
+
+def __towers_location_dataframes() -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Returns towers location(s) dataframes."""
     municipality_df = pd.read_csv("data/municipios_tower.csv", engine="pyarrow")
     health_areas_df = pd.read_csv("data/as_havana_tower.csv", engine="pyarrow")
     return municipality_df, health_areas_df
 
-def get_towers(location : str) -> List[str]: # returns towers list
+def get_towers(location : str) -> List[str]:
+    """
+    Returns towers list.
+    """
     pass
-    
+
+def get_collection(collection_name : str) -> List[str]:
+    """
+    Returns collection.
+    """
+    pass
+
+def filter(set, *filters):
+    """
+    Returns a new set filtered by filters
+    """
+    pass
+
+
+#region set operations
+
+def union(A, B):
+    """
+    Returns two sets union.
+    """
+    pass
+
+def intersection(A, B):
+    """
+    Returns two sets intersections.
+    """
+    pass
+
+def difference(A, B):
+    """
+    Returns two sets difference. (parameters order)
+    """
+    pass
+
+#endregion
+
+
