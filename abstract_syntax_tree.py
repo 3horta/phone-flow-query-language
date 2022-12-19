b4d27057b37ad94266ed53d5c1f12d67d24e9bb6
hbm99 <ernestoblancomalmierca@gmail.com>
2022-12-19 10:22:06 -0500

    Add evaluate

--- abstract_syntax_tree.py
+++ abstract_syntax_tree.py
@@ -1,25 +1,34 @@
 
 class Expression: 
-    pass
+    def evaluate(self):
+        pass
 
 class FilterOp(Expression):
     def __init__(self, op, registers, predicates) -> None:
         self.op = op
         self.registers = registers
         self.predicates = predicates
+    def evaluate(self):
+        pass
 
 class Predicate(Expression):
     def __init__(self, predicate) -> None:
         self.predicate = predicate
+    def evaluate(self):
+        pass
 
 class TimePredicate(Predicate):
     def __init__(self, predicate, start_date, end_date) -> None:
         super().__init__(predicate)
         self.start_date = start_date
         self.end_date = end_date
+    def evaluate(self):
+        pass
         
 class LocationPredicate(Predicate):
     def __init__(self, predicate, location) -> None:
         super().__init__(predicate)
         self.location = location
+    def evaluate(self):
+        pass
         
