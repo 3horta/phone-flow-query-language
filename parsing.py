26c38debf79b3c39278df94817d027155cb4b0e4
Elena <elenitarh01@gmail.com>
2022-12-28 10:12:39 -0500

    Add parsing methods for new version of grammar

--- parsing.py
+++ parsing.py
@@ -5,13 +5,29 @@ from abstract_syntax_tree import *
 # -----------------------------------------------------------------------------
 #   Grammar
 #
-#   Program          : group Register_set by Collection_list; 
-#                    | users ( Register_set ) ;
-#                    | towers ( Register_set ) ; 
-#                    | count ( Register_set ) ; 
-#                    | Register_set ;
+#   Program          : SuperType id = Expression ;
+#                    | id = Expression ;
+#
+#   SuperType        : list(Type) 
+#                    | ClusterSet 
+#                    | Type
+#
+#   ClusterSet       : clusterset(string, ClusterSet) 
+#                    | clusterset(string, registerset)
+#
+#   Type             : registerset 
+#                    | int 
+#                    | string 
+#                    | date
+#
+#   Expression       : group Register_set by Collection_list
+#                    | users ( Register_set )
+#                    | towers ( Register_set )
+#                    | count ( Register_set )
+#                    | Register_set
 # 
-#   Register_set     : ALL 
+#   Register_set     : id 
+#                    | ALL
 #                    | filter Register_set by Predicate_list
 #
 #   Collection_list  : Collection, Collection_list
@@ -19,44 +35,99 @@ from abstract_syntax_tree import *
 # 
 #   Collection       : { provinces }
 #                    | { municipalities }
+#                    | id
 # 
 #   Predicate_list   : Predicate, Predicate_list 
 #                    | Predicate 
 #
 #   Predicate        : time ( date , date )
 #                    | location ( string )
-#
+#                    | id
 # -----------------------------------------------------------------------------
 
 # Write functions for each grammar rule which is
 # specified in the docstring.
+
+def p_variable(p):
+    '''
+    Program : SuperType ID EQUAL Expression END
+            | ID EQUAL Expression END
+    '''
+    if len(p) == 6:
+        p[0] = VariableDeclaration(p[1], p[2], p[4])
+    elif len(p) == 5:
+        p[0] = VariableAssignment(p[1], p[3])
+
+def p_supertype_list(p):
+    '''
+    SuperType : TYPE LPAREN Type RPAREN
+    '''
+    p[0] = p[1] + p[2] + p[3] + p[4]
+    
+def p_supertype_clusterset(p):
+    '''
+    SuperType : ClusterSet
+    '''
+    p[0] = p[1]
+
+def p_supertype_type(p):
+    '''
+    SuperType : Type
+    '''
+    p[0] = p[1]
+
+def p_clusterset_clusterset(p):
+    '''
+    ClusterSet : TYPE LPAREN TYPE COMMA ClusterSet RPAREN
+    '''
+    p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
+    
+def p_clusterset_registerset(p):
+    '''
+    ClusterSet : TYPE LPAREN TYPE COMMA TYPE RPAREN
+    '''
+    p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
+    
+def p_type(p):
+    '''
+    Type : TYPE
+    '''
+    p[0] = p[1]
+
 def p_group(p):
     '''
-    Program : GROUP Register_set BY Collection_list END
+    Expression : GROUP Register_set BY Collection_list
     '''
     p[0] = GroupOp(p[2], p[4])
     
 def p_users(p):
     '''
-    Program : USER LPAREN Register_set RPAREN END
+    Expression : USER LPAREN Register_set RPAREN
     '''
     p[0] = Users(p[3])
 
 def p_towers(p):
     '''
-    Program : TOWER LPAREN Register_set RPAREN END
+    Expression : TOWER LPAREN Register_set RPAREN
     '''
     p[0] = Towers(p[3])
     
 def p_count(p):
     '''
-    Program : COUNT LPAREN Register_set RPAREN END
+    Expression : COUNT LPAREN Register_set RPAREN
     '''
     p[0] = Count(p[3])
 
-def p_program_register_set(p):
+def p_expression_register_set(p):
     '''
-    Program : Register_set END
+    Expression : Register_set
+    '''
+    p[0] = p[1]
+    
+    
+def p_registerset_id(p):
+    '''
+    Register_set : ID
     '''
     p[0] = p[1]
     
@@ -89,10 +160,13 @@ def p_collection_list(p):
         
 def p_collection(p):
     '''
-    Collection  : LBRACE PROV RBRACE
+    Collection  : ID
+                | LBRACE PROV RBRACE
                 | LBRACE MUN RBRACE
     '''
-    if(p[2] == 'PROVINCES'):
+    if len(p) == 2:
+        p[0] = p[1]
+    elif p[2] == 'PROVINCES':
         p[0]= ProvincesCollection()
     elif p[2] =='MUNICIPALITIES':
         p[0]= MunicipalitiesCollection()
@@ -111,8 +185,11 @@ def p_predicate(p):
     '''
     Predicate  : TIME LPAREN DATE COMMA DATE RPAREN
                | LOCATION LPAREN STRING RPAREN
+               | ID
     '''
-    if(p[1] == 'time'):
+    if len(p) == 2:
+        p[0] = p[1]
+    elif p[1] == 'time':
         p[0]= TimePredicate(p[3], p[5])
     elif p[1] =='location':
         p[0]= LocationPredicate(p[3])
