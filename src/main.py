472c446a839c24b3d27bff8b69f684ac0f2d5d63
hbm99 <ernestoblancomalmierca@gmail.com>
2023-01-06 16:49:42 -0500

    Update UI message

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from lang.context import Context
 from lang.semantic_checker import SemanticChecker
 from parsing import parser
 
-print("Insert file name ( with extension '.pfql' ) to run: ")
+print("Insert file name ( '.pfql' extension ) to run: ")
 file_name = input()
 
 with open('tester/' + file_name + '.pfql', 'r') as file:
